914dd0ab6b174173e0f17469b9965f281c8c4876
omshinde <omshinde1706@gmail.com>
2025-04-01 23:31:26 +0530

    Basic Java Chat Server Initial Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "io.nemesis"
@@ -12,6 +13,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "io.nemesis.Application"
 }
 
 tasks.test {
